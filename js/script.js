d638b24f09b37125836025b0cf26e7a114601527
Joe-Valente <112267629+Joe-Valente@users.noreply.github.com>
2022-11-25 12:08:38 -0600

    Trying to make js work properly

--- js/script.js
+++ js/script.js
@@ -1,17 +1,32 @@
-// Initialize and add the map
 function initMap() {
-  // The location of Uluru
-  const uluru = { lat: -25.344, lng: 131.031 };
-  // The map, centered at Uluru
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: uluru,
-  });
-  // The marker, positioned at Uluru
-  const marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
-  });
+    var mapCenter = new google.maps.LatLng(40.23175547478395, -89.49721984005868);
+
+    let map = new google.maps.Map(document.getElementById('map'), {
+        center: mapCenter,
+        zoom: 4,
+        mapTypeId: 'satellite'
+    });
+
+    new google.maps.Marker({
+        position: new google.maps.LatLng(41.835252597261906, -87.62579957391424),
+        map,
+        label: "My School",
+        animation: google.maps.Animation.DROP
+    });
+    
+    new google.maps.Marker({
+        position: new google.maps.LatLng(41.843623955764464, -87.64823815014381),
+        map,
+        label: "My City",
+        animation: google.maps.Animation.DROP
+    });
+
+    new google.maps.Marker({
+        position: new google.maps.LatLng(41.51121443857638, -87.96882104828151),
+        map,
+        label: "My Hometown",
+        animation: google.maps.Animation.DROP
+    });
 }
 
 window.initMap = initMap;
